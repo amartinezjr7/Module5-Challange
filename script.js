76289a811f12396bc657b34df04f40b2ef1032c1
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-01-08 18:48:19 -0800

    created gitignore and 9am and 10am
    I managed to create a 9 and 10 am row and save the date in local storage

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 var todaysDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(todaysDate);
+
+$(document).ready({
+
+})
+
+function tracker(){
+    var currentTime = moment().hour();
+
+}
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10, description").val(localStorage.getItem("#hour-10"));
