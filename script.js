90ec54a98cc788ac054c6d6c6f0683fc6e6f21cd
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-01-08 19:50:44 -0800

    Fixed the save issue
    data can now be added and saved to local storage. Also added all rows

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 var todaysDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(todaysDate);
 
-$(document).ready({
+$(document).ready(function(){
+
+        $(".saveBtn").on("click", function(){
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+            localStorage.setItem( time, text);
+        })
+    
+
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour12 .description").val(localStorage.getItem("hour11"));
+    $("#hour13 .description").val(localStorage.getItem("hour12"));
+    $("#hour14 .description").val(localStorage.getItem("hour13"));
+    $("#hour15 .description").val(localStorage.getItem("hour14"));
+    $("#hour16 .description").val(localStorage.getItem("hour15"));
+    $("#hour17 .description").val(localStorage.getItem("hour16"));
+    $("#hour18 .description").val(localStorage.getItem("hour17"));
+
 
 })
 
-function tracker(){
-    var currentTime = moment().hour();
-
-}
-
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10, description").val(localStorage.getItem("#hour-10"));
+    
+        
